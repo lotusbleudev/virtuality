40c965982d184e05eb044329ce617744a91a8816
lotusbleudev <blueboisd@gmail.com>
2021-09-17 11:16:36 +0200

    ok

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ Encore
      */
     .addEntry('app', './assets/app.js')
     .addEntry('menu', './assets/menu.js')
+    .addEntry('home', './assets/home.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
