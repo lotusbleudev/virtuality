349761e01c077774ea0d11077b793e727119b3d4
Mounif Abdoulatif <mounif2012@gmail.com>
2021-09-17 16:55:47 +0200

    registration_form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -26,11 +26,11 @@ class RegistrationFormType extends AbstractType
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Veuillez saisir un mot de passe',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Votre mot de passe devrai contenir au moins {{ limit }} characters',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
