9899b4499f85b5f92580c5a6e35ab87887a15553
Souxer <88085700+Souxer@users.noreply.github.com>
2021-09-21 09:41:37 +0200

    nav fini + effect

--- assets/menu.js
+++ assets/menu.js
@@ -4,10 +4,15 @@ import './styles/menu.scss';
 
 const menuButton = document.querySelector('.menu-button');
 const menuOverlay = document.querySelector('.menu-overlay');
+const mainWidth = document.querySelector('.main-width');
+const footerWidth = document.querySelector('.footer-width');
+
 // const backgroundOverlay = document.querySelector('.background-overlay');
 
 menuButton.addEventListener('click', function() {
     menuButton.classList.toggle('active');
     menuOverlay.classList.toggle('open');
+    mainWidth.classList.toggle('reduce');
+    footerWidth.classList.toggle('reduce');
     // backgroundOverlay.classList.toggle('bg-overlay');
 });
