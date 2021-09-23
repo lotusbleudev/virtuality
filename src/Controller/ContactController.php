eccbf4be1edbe15f956c0946ab27dac0cdcb9a08
Mounif Abdoulatif <mounif2012@gmail.com>
2021-09-23 20:47:42 +0200

    Contact is working, styliser le template(email_reçu) + ajout du map dans contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,17 +4,44 @@ namespace App\Controller;
 
 use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact')]
-    public function index()
+    public function index(Request $request, \Swift_Mailer $mailer)
     {
         $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $contact = $form->getData();
+
+            //Ici nous enverrons le mail
+            // dd($contact);
+
+            $message = (new \Swift_Message('Vous avez reçu un nouveau message'))
+                ->setFrom($contact['email'])
+                ->setTo('virtuality255@gmail.com')
+                ->setBody(
+                    $this->renderView(
+                        'email/email_reçu.html.twig', compact('contact')
+                    ),
+                    'text/html' // on lui dit que c'est des text/html car on veut qu'il prend en compte les balises dans notre vue
+
+                )
+            ; 
+            // on envoie le message
+            $mailer->send($message);
+
+            $this->addFlash('success', 'Votre message a été envoyez avec succès'); // Il faut créer un template Flash_message
+
+            return $this->redirectToRoute('contact');
+        }
 
         return $this->render('contact/index.html.twig', [
-            'contact_form' => $form->createView(),
+            'contact_form' => $form->createView()
         ]);
     }
 }
