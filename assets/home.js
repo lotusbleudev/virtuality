ba0c3a509200891d90c9bd13d725a675e007fa2b
Mounif Abdoulatif <mounif2012@gmail.com>
2021-09-23 15:15:35 +0200

    ..

--- assets/home.js
+++ assets/home.js
@@ -1,1 +1,35 @@
-import './styles/home.scss';
+import "./styles/home.scss";
+
+
+// function isElementInViewport(elem) {
+//     var $elem = $(elem);
+
+//     // Get the scroll position of the page.
+//     var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('animate') != -1) ? 'body' : 'html');
+//     var viewportTop = $(scrollElem).scrollTop();
+//     var viewportBottom = viewportTop + $(window).height();
+
+//     // Get the position of the element on the page.
+//     var elemTop = Math.round( $elem.offset().top );
+//     var elemBottom = elemTop + $elem.height();
+
+//     return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
+// }
+
+// // Check if it's time to start the animation.
+// function checkAnimation() {
+//     var $elem = $('.box1 .animate');
+
+//     // If the animation has already been started
+//     if ($elem.hasClass('start')) return;
+
+//     if (isElementInViewport($elem)) {
+//         // Start the animation
+//         $elem.addClass('start');
+//     }
+// }
+
+// // Capture scroll events
+// $(window).scroll(function(){
+//     checkAnimation();
+// });
--- src/Form/ReservationType.php
+++ src/Form/ReservationType.php
@@ -4,7 +4,9 @@ namespace App\Form;
 
 use App\Entity\Reservation;
 use App\Entity\User;
+use DateTime;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
@@ -16,16 +18,12 @@ class ReservationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            // ->add('user',  CollectionType::class, [
-            //     'entry_type' => User::class,
-            //     'entry_options' => array('label' => false)
-            //     ])
             ->add('date', DateTimeType::class, [
-                'date_widget' => 'single_text'
+                'date_widget' => 'single_text',
+                'hours' => range(9,21),
+                'minutes' => [00]
             ])
-            ->add('nb_joueurs')
-            
-            ->add('prix') // créer un algo pour dynamiser le prix en fonction du jour: jour normal ou week-end/férié
+            ->add('nb_joueurs') // créer un algo pour dynamiser le prix en fonction du jour: jour normal ou week-end/férié
         ;
     }
 
