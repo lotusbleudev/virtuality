bd40d5f7af53ab1eba77e6038bf5f23fbada4027
lotusbleudev <blueboisd@gmail.com>
2021-09-24 12:25:07 +0200

    background gradient

--- src/Controller/BackofficeController.php
+++ src/Controller/BackofficeController.php
@@ -2,15 +2,14 @@
 
 namespace App\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[IsGranted("ROLE_ADMIN")]
+
 class BackofficeController extends AbstractController
 {
-    #[Route('/backoffice', name: 'backoffice')]
+    #[Route('/admin/backoffice', name: 'backoffice')]
     public function index(): Response
     {
         return $this->render('backoffice/index.html.twig', [
