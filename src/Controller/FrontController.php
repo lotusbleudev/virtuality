82c578810a8b06d48ad9db76ec324d113501a551
lotusbleudev <blueboisd@gmail.com>
2021-09-25 13:18:21 +0200

    front

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\JeuxRepository;
+use App\Repository\PrixRepository;
+use App\Repository\TournoisRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,26 +12,26 @@ use Symfony\Component\Routing\Annotation\Route;
 class FrontController extends AbstractController
 {
     #[Route('/tarifs', name: 'tarifs')]
-    public function tarifs(): Response
+    public function tarifs(PrixRepository $pr): Response
     {
-        return $this->render('front/index.html.twig', [
-            'controller_name' => 'FrontController',
+        return $this->render('front/tarifs.html.twig', [
+            "tarifs" => $pr->findAll()
         ]);
     }
 
     #[Route('/jeux', name: 'jeux')]
-    public function jeux(): Response
+    public function jeux(JeuxRepository $jr): Response
     {
-        return $this->render('front/index.html.twig', [
-            'controller_name' => 'FrontController',
+        return $this->render('front/jeux.html.twig', [
+            "jeux" => $jr->findAll()
         ]);
     }
 
     #[Route('/tournois', name: 'tournois')]
-    public function tournois(): Response
+    public function tournois(TournoisRepository $tr): Response
     {
-        return $this->render('front/index.html.twig', [
-            'controller_name' => 'FrontController',
+        return $this->render('front/tournois.html.twig', [
+            "tournois" => $tr->findAll()
         ]);
     }
 }
