15ea632753d6190f228cdbccec8cc9d049cb76b5
Souxer <88085700+Souxer@users.noreply.github.com>
2021-09-27 11:56:20 +0200

    changement nav

--- assets/app.js
+++ assets/app.js
@@ -10,7 +10,8 @@ import "./styles/app.scss";
 
 // start the Stimulus application
 import "./bootstrap";
-
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
 
 
 
